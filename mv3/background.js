29337f1e5299e361cbdd51d4f421c3cb0487553b
rtldg <rtldg@protonmail.com>
2025-05-07 03:58:25 +0000

    v1.1 fail test

---
+++ mv3/background.js
@@ -0,0 +1,4 @@
+// some dummy background.js code so hopefully the extension stays loaded so the ruleset1.json works....
+browser.runtime.onStartup.addListener(() => {
+	console.log("hello :)");
+});
