68ea91388dc9fe50d4b3cc63c03e624035d7e5b3
rtldg <rtldg@protonmail.com>
2025-05-10 06:33:16 +0000

    v1.1 was a stupid fail and is unnecessary because firefox is a broken piece of shit and i had to make a new profile because it's fucked for no reason

--- mv3/background.js
+++
@@ -1,4 +0,0 @@
-// some dummy background.js code so hopefully the extension stays loaded so the ruleset1.json works....
-browser.runtime.onStartup.addListener(() => {
-	console.log("hello :)");
-});
