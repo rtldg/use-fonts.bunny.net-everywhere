847b93c90da0b5f5f14bc6cc43114a8d252f8297
David Nordvall <david.nordvall@doitsolutions.se>
2026-08-31 16:45:59 +0200

    Refactor the code to handle both CSP and CSP report-only headers in the same set of response headers.

--- mv2/background.js
+++ mv2/background.js
@@ -1,47 +1,46 @@
 function headersListener(details) {
-	// Only add fonts.bunny.net to the CSP header if a CSP header already is present.
-	const cspHeaderName = details.responseHeaders.filter(h => h.name.toLowerCase() === "content-security-policy-report-only").length > 0
-		? "content-security-policy-report-only"
-		: "content-security-policy"
-	const csp = details.responseHeaders
-		.filter(h => h.name.toLowerCase() === cspHeaderName)
-		.map(h => h.value)[0];
+	return {
+		...details,
+		responseHeaders: [
+			...details.responseHeaders.filter(h => !["content-security-policy", "content-security-policy-report-only"].includes(h.name.toLowerCase())),
+			...processCspHeader(details.responseHeaders, "content-security-policy"),
+			...processCspHeader(details.responseHeaders, "content-security-policy-report-only")
+		]
+	};
+}
+
+function processCspHeader(responseHeaders, name) {
+	const csp = responseHeaders.filter(h => h.name.toLowerCase() === name).map(h => h.value)[0];
 	if (csp) {
 		// Parse the CSP header in order to rewrite the parts regarding the style and font sources.
 		const parsedCsp = csp
 			.split(';')
-			.map(pd => pd.trim())
 			.map(pd => {
-				const parts = pd.split(" ");
-				return { directive: parts[0].trim(), value: parts.slice(1).map(v => v.trim()).join(" ") };
+				const parts = pd.trim().split(" ").map(v => v.trim());
+				return { directive: parts[0], value: parts.slice(1).join(" ") };
 			});
 		// Add Fonts Bunny to style and font sources. Note that we cannot remove the Google Fonts sources
 		// since that will prevent the rewriting of the actual style and font requests. I.e. the CSP need
 		// to contain _both_ Google Fonts and Fonts Bunny sources for this to work.
-		const updatedCsp = parsedCsp
-			.map(pd => {
-				switch (pd.directive) {
-					case "style-src":
-						return { ...pd, value: `${pd.value} https://fonts.bunny.net` };
-					case "font-src":
-						return { ...pd, value: `${pd.value} data: https://fonts.bunny.net`}
-					default:
-						return pd;
-				}
-			});
-
-		// Finally return the updated details object with the original CSP header replaced with
-		// our updated header.
-		return {
-			...details,
-			responseHeaders: [
-				...details.responseHeaders.filter(h => h.name.toLowerCase() !== cspHeaderName),
-				{ name: cspHeaderName, value: updatedCsp.map(pd => `${pd.directive} ${pd.value}`).join('; ') }
-			]
-		};
+		return [{
+			name,
+			value: parsedCsp
+				.map(pd => {
+					switch (pd.directive) {
+						case "style-src":
+							return { ...pd, value: `${pd.value} https://fonts.bunny.net` };
+						case "font-src":
+							return { ...pd, value: `${pd.value} data: https://fonts.bunny.net`}
+						default:
+							return pd;
+					}
+				})
+				.map(pd => `${pd.directive} ${pd.value}`)
+				.join('; ')
+		}];
 	}
 
-	return details;
+	return [];
 }
 
 function reqListener(details) {
