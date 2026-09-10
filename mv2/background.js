fd7cc025350bf5b102bbbcb8de5c2af3a530f718
David Nordvall <david.nordvall@doitsolutions.se>
2026-09-10 05:35:32 +0200

    Add Fonts Bunny sources to the CSP header, if necessary. (#1)
    Co-authored-by: rtldg <55846624+rtldg@users.noreply.github.com>

--- mv2/background.js
+++ mv2/background.js
@@ -1,3 +1,52 @@
+function headersListener(details) {
+	return {
+		...details,
+		responseHeaders: details.responseHeaders.map(processCspHeader)
+	};
+}
+
+function processCspHeader(h) {
+	const is_csp = ["content-security-policy", "content-security-policy-report-only"].includes(h.name.toLowerCase());
+	if (is_csp) {
+		// Parse the CSP header in order to rewrite the parts regarding the style and font sources.
+		const parsedCsp = h.value
+			.split(';')
+			.map(pd => {
+				const parts = pd.trim().split(" ").map(v => v.trim());
+				return { directive: parts[0], value: parts.slice(1).join(" ") };
+			});
+		// Add Fonts Bunny to style and font sources. Note that we cannot remove the Google Fonts sources
+		// since that will prevent the rewriting of the actual style and font requests. I.e. the CSP need
+		// to contain _both_ Google Fonts and Fonts Bunny sources for this to work.
+		return {
+			name: h.name,
+			value: parsedCsp
+				.map(pd => {
+					switch (pd.directive) {
+						case "style-src":
+							if (pd.value.includes("https://fonts.googleapis.com")) {
+								return { ...pd, value: `${pd.value} https://fonts.bunny.net` };
+							} else {
+								return pd;
+							}
+						case "font-src":
+							if (pd.value.includes("https://fonts.gstatic.com")) {
+								return { ...pd, value: `${pd.value} data: https://fonts.bunny.net`}
+							} else {
+								return pd;
+							}
+						default:
+							return pd;
+					}
+				})
+				.map(pd => `${pd.directive} ${pd.value}`)
+				.join('; ')
+		};
+	} else {
+		return h;
+	}
+}
+
 function reqListener(details) {
 	let url = new URL(details.url);
 	if (url.hostname == "fonts.googleapis.com" && url.pathname.startsWith("/css"))
@@ -10,12 +59,14 @@ function reqListener(details) {
 	return;
 }
 
+browser.webRequest.onHeadersReceived.addListener(
+	headersListener,
+	{ urls: ["<all_urls>"], types: ["main_frame"] },
+	["responseHeaders", "blocking"]
+);
+
 browser.webRequest.onBeforeRequest.addListener(
 	reqListener,
-	{
-		urls: [
-			"<all_urls>",
-		],
-	},
+	{ urls: ["<all_urls>"], types: ["stylesheet"] },
 	["blocking"]
 );
